5398c84841d03ea194239f8ac90e258ac886d41a
Jhuguenin-code <jhuguenin@uchicago.edu>
2023-03-06 01:46:55 +0000

    (add listing form works) Sublet rent input=desired sublet rent, made delete/update listing forms only available if signed in, non-signed in users can only see modeled sublet rent and not user inputted sublet/current rent

--- app/models/interest.rb
+++ app/models/interest.rb
@@ -9,4 +9,5 @@
 #  user_id    :integer
 #
 class Interest < ApplicationRecord
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -15,4 +15,8 @@ class User < ApplicationRecord
   validates :email, :uniqueness => { :case_sensitive => false }
   validates :email, :presence => true
   has_secure_password
+
+  def full_name
+    return first_name + " " + last_name
+  end
 end
