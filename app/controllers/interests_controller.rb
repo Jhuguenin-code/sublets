4388fc7d469c98aa0451d6b816a2b640f5fb6f84
Jhuguenin-code <jhuguenin@uchicago.edu>
2023-03-07 06:00:36 +0000

    Changed interest index page to dropdown menu

--- app/controllers/interests_controller.rb
+++ app/controllers/interests_controller.rb
@@ -20,11 +20,13 @@ class InterestsController < ApplicationController
   def create
     the_interest = Interest.new
     the_interest.listing_id = params.fetch("query_listing_id")
-    the_interest.user_id = params.fetch("query_user_id")
+    the_interest.user_id = session.fetch(:user_id)
+
+    #the_interest.user_id = params.fetch("query_user_id")
 
     if the_interest.valid?
       the_interest.save
-      redirect_to("/interests", { :notice => "Interest created successfully." })
+      redirect_to("/interests", { :notice => "This listing is now favorited! Place a bid!" })
     else
       redirect_to("/interests", { :alert => the_interest.errors.full_messages.to_sentence })
     end
